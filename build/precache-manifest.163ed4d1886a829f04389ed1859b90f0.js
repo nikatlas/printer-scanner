a8a513b43a957d908d8fd05537ff5851b7d0d9ee
Nikatlas <nikatlas@gmail.com>
2020-05-02 21:13:42 +0300

    COmmit buuild

---
+++ build/precache-manifest.163ed4d1886a829f04389ed1859b90f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cad37df34c4d07932674c93221839f11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38016b6d829cee5b5077",
+    "url": "/static/css/main.e2ca96e0.chunk.css"
+  },
+  {
+    "revision": "aa9241aa26aaaefc6669",
+    "url": "/static/js/2.6681e83f.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/static/js/2.6681e83f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38016b6d829cee5b5077",
+    "url": "/static/js/main.169783a0.chunk.js"
+  },
+  {
+    "revision": "c596147e57a9961a32e6",
+    "url": "/static/js/runtime-main.80db0bd1.js"
+  }
+]);
